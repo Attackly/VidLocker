6c6995cdbde29f89fea2e286eb07aab7be692213
jan <Attackly@outlook.de>
2024-08-26 19:37:19 +0200

    Initial Commit.
    Downloading works.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#[deny(warnings)]
+
+mod routes;
+mod func;
+
+use axum::{
+    Router,
+    routing::{get, post}
+};
+use routes::misc::check_system_handler;
+use crate::routes::video::simple_download_handler;
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new()
+        .route("/test", get(check_system_handler))
+        .route("/api/downloadVideo", post(simple_download_handler));
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0:3000")).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap()
+}
