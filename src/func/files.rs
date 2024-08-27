38a1c1c74e08f7162a9a73fdf4dcb69b7b1fd1e6
jan <Attackly@outlook.de>
2024-08-27 21:17:01 +0200

    Start on the Remove dir function and Handler

--- src/func/files.rs
+++ src/func/files.rs
@@ -21,3 +21,33 @@ pub async fn dir_create<'a>(path: String) -> Result<&'a str, u8> {
 
 
 }
+
+pub async fn dir_delete<'a>(path: String) -> Result<&'a str, u8> {
+    if path.find("..").is_some() {
+        return Err(1)
+    }
+
+    let path = PathBuf::from("output/".to_owned() + &*path );
+
+    match fs::remove_dir(path).await {
+        Ok(_) => {
+            Ok("Dir deleted")
+        }
+        Err(_) => {
+            Err(2)
+        }
+    }
+}
+
+
+#[tokio::test]
+async fn test_dir_creation() {
+    assert!(dir_create("TESTS/RUST".to_string()).await.is_ok())
+}
+
+#[tokio::test]
+async fn test_dir_deletion() {
+    if PathBuf::from("TEST/RUST").exists() {
+        assert!(dir_delete("TEST/RUST".to_string()).await.is_ok())
+    }
+}
