ed8a936aa69816eec1fd57782282e3c28171dd7d
Attackly <Attackly@outlook.de>
2025-01-01 21:34:59 +0100

    It now Downloads again.

--- src/func/video.rs
+++ src/func/video.rs
@@ -1,8 +1,15 @@
 use std::path::PathBuf;
 use std::thread;
-use youtube_dl::{YoutubeDl};
+use youtube_dl::YoutubeDl; // Assuming you're using the youtube-dl crate.
+
 pub fn download_video_simple_ydl(link: String) {
+    let folder_path = "/home/jan/Projects/VidLocker/output";
+    
+    // Download the video in a separate thread
     thread::spawn(move || {
-        YoutubeDl::new(link).download_to(PathBuf::from("output/")).unwrap();
+        match YoutubeDl::new(link).download_to(PathBuf::from(folder_path)) {
+            Ok(_) => println!("Video downloaded successfully to '{}'", folder_path),
+            Err(e) => eprintln!("Failed to download video: {}", e),
+        }
     });
 }
