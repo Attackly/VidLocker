59aa2c828902376e3a1036594e4641c03feeec37
Jan <Attackly@outlook.de>
2025-01-12 15:14:44 +0100

    Format Preperations.rs

--- backend/src/func/preperations.rs
+++ backend/src/func/preperations.rs
@@ -1,22 +1,26 @@
-use std::{fs, io};
 use dotenvy::dotenv;
-use sqlx::postgres::PgPoolOptions;   
+use sqlx::postgres::PgPoolOptions;
+use std::{fs, io};
 
-pub fn create_output_dir() -> (){
+pub fn create_output_dir() -> () {
     match fs::create_dir("output") {
         Ok(_) => (),
         Err(e) if e.kind() == io::ErrorKind::AlreadyExists => {
             ();
         }
-        Err(e) => panic!("There was an error! {e}")
+        Err(e) => panic!("There was an error! {e}"),
     }
 }
 
-pub async fn prepare_database() -> (){
+pub async fn prepare_database() -> () {
     dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    let pool = PgPoolOptions::new().max_connections(30).connect(&database_url).await.expect("Failed to connect to database");
+    let pool = PgPoolOptions::new()
+        .max_connections(30)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to database");
     sqlx::migrate!("./migrations")
         .run(&pool)
         .await
