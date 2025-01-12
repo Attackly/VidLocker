ce2bcee1dd83598a2a0f45238a5acf4d815f9b1e
Jan <Attackly@outlook.de>
2025-01-12 19:29:22 +0100

    Savepoint it now works. Docker is a handfull.

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -2,18 +2,17 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+  // interface Locals {}
+  // interface PageData {}
+  // interface Error {}
+  // interface Platform {}
 }
 
 // global.d.ts
 declare global {
-	interface Window {
-	  YT: any;
-	}
+  interface Window {
+    YT: any;
   }
-  
-  export {};
-  
+}
+
+export {};
