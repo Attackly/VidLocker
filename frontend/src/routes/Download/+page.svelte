06b17ae9f83e7bf9a2c4277670beec5b89e367ae
Jan <Attackly@outlook.de>
2025-02-16 16:51:26 +0100

    Continue Frontend
    Continue Frontend to make it possible to decide where a Video ssohuld download to

--- frontend/src/routes/Download/+page.svelte
+++ frontend/src/routes/Download/+page.svelte
@@ -13,6 +13,8 @@
     let video_published_at: Date | null = null;
     let video_published_str: string = "";
     let disabled = true;
+    let isOpen = true;
+    let data: any[] = [{ Name: "Baerys newyear karaoke", Size: 1000 }];
     function url_verify() {
         // ViewKey
         if (url.length == 11) {
@@ -178,6 +180,66 @@
             <p class="text-red-500">Error: Video not found.</p>
         {/if}
     </div>
+
+    <div class="lg:w-1/3 mt-8 card p-4">
+        <div class="card-header">
+            <h3>Collapsible Card</h3>
+            <button on:click={() => (isOpen = !isOpen)}>
+                {#if isOpen}
+                    🔼
+                {:else}
+                    🔽
+                {/if}
+            </button>
+        </div>
+        {#if isOpen}
+            {#if data.length > 0}
+                <table class="w-full">
+                    <thead class="">
+                        <tr>
+                            <th class="px-4 py-2 text-left">Name</th>
+                            <th class="px-4 py-2 text-left">Size</th>
+                            <th class="px-4 py-2 text-left">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {#each data as row}
+                            <tr>
+                                <td class="px-4 py-2">{row.Name}</td>
+                                <td class="px-4 py-2">{row.Size}</td>
+                                <td
+                                    ><button
+                                        class="bg-red-700 p-1 rounded"
+                                        id="test"
+                                    >
+                                        <svg
+                                            class="w-5 h-5"
+                                            fill="#ffffff"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 448 512"
+                                            ><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path
+                                                d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
+                                            /></svg
+                                        >
+                                    </button>
+                                </td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+            {:else}
+                <div
+                    class="p-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
+                    role="alert"
+                >
+                    Nothing to display yet. Better Start Downloading
+                </div>
+            {/if}
+        {/if}
+    </div>
+    {#if isOpen}
+        <div class="lg:w-1/3 mt-8 card p-4"></div>
+    {/if}
 
     <div class="lg:w-1/3 mt-8 card p-4">
         <button
